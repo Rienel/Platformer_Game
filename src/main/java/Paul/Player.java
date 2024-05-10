3415ea6b8595b86c6c9eaba39253fca46e2ae5f5
Rienel <rieub123@gmail.com>
2024-05-10 23:22:21 +0800

    Merged Code from Paul

---
+++ src/main/java/Paul/Player.java
@@ -0,0 +1,12 @@
+package Paul;
+
+
+
+import javafx.scene.paint.Color;
+
+public class Player extends Entity{
+    public Player(int x, int y, int w, int h, Color color) {
+        super(x, y, w, h, color);
+    }
+
+}
